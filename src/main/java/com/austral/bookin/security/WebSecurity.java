e1ae848a4983c93a1ee665737a743b70468b8d04
Brian Froschauer <bafroschauer@gmail.com>
2020-08-19 20:04:21 -0300

    fix integration tests context

--- src/main/java/com/austral/bookin/security/WebSecurity.java
+++ src/main/java/com/austral/bookin/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.austral.bookin.security;
 
 import com.austral.bookin.service.authentication.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
--- src/test/java/com/austral/bookin/controller/integration/SignupControllerTest.java
+++ src/test/java/com/austral/bookin/controller/integration/SignupControllerTest.java
@@ -7,6 +7,7 @@ import com.austral.bookin.security.WebSecurity;
 import com.austral.bookin.service.authentication.AuthenticationService;
 import com.austral.bookin.service.signup.SignupService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,19 +16,25 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = SignupController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SignupControllerTest {
 
 

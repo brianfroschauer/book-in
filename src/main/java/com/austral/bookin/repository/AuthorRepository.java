64389339e5bd0cd7665580a11f2d311ac4167e6d
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2020-09-09 19:43:24 -0300

    Adding method to get book's authors

--- src/main/java/com/austral/bookin/repository/AuthorRepository.java
+++ src/main/java/com/austral/bookin/repository/AuthorRepository.java
@@ -3,5 +3,12 @@ package com.austral.bookin.repository;
 import com.austral.bookin.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AuthorRepository extends JpaRepository<Author, Long>, JpaSpecificationExecutor<Author> {}
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<Author, Long>, JpaSpecificationExecutor<Author> {
+
+    @Query(value = "select * from author a where a.id in (select ba.author_id from book_author ba where ba.book_id = ?1)", nativeQuery = true)
+    List<Author> findAllByBook(Long id);
+}

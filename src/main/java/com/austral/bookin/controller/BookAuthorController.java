0580de6eee349309254f92549887600afb2af48f
katiacammisa <43763141+katiacammisa@users.noreply.github.com>
2020-09-09 19:46:55 -0300

    Adding method to get book's authors (#19)

--- src/main/java/com/austral/bookin/controller/BookAuthorController.java
+++ src/main/java/com/austral/bookin/controller/BookAuthorController.java
@@ -1,7 +1,10 @@
 package com.austral.bookin.controller;
 
+import com.austral.bookin.dto.author.AuthorDTO;
 import com.austral.bookin.dto.book.BookDTO;
+import com.austral.bookin.entity.Author;
 import com.austral.bookin.entity.Book;
+import com.austral.bookin.service.author.AuthorService;
 import com.austral.bookin.service.book.BookService;
 import com.austral.bookin.util.ObjectMapper;
 import com.austral.bookin.util.ObjectMapperImpl;
@@ -16,10 +19,12 @@ import java.util.List;
 public class BookAuthorController {
 
     private final BookService bookService;
+    private final AuthorService authorService;
     private final ObjectMapper objectMapper;
 
-    public BookAuthorController(BookService bookService) {
+    public BookAuthorController(BookService bookService, AuthorService authorService) {
         this.bookService = bookService;
+        this.authorService = authorService;
         objectMapper = new ObjectMapperImpl();
     }
 
@@ -28,4 +33,11 @@ public class BookAuthorController {
         final List<Book> books = bookService.findByAuthor(id);
         return ResponseEntity.ok(objectMapper.map(books, BookDTO.class));
     }
+
+    @GetMapping("/books/{id}/authors")
+    public ResponseEntity<List<AuthorDTO>> findByBook(@PathVariable Long id) {
+        final List<Author> authors = authorService.findByBook(id);
+        return ResponseEntity.ok(objectMapper.map(authors, AuthorDTO.class));
+    }
+
 }

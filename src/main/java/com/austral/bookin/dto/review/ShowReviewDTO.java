4c331b1377853939191ba2273048afb189165cf9
Katia Cammisa <katia.cammisa@ing.austral.edu.ar>
2020-10-01 17:23:16 -0300

    Adding ShowReviewDTO for review visualization

---
+++ src/main/java/com/austral/bookin/dto/review/ShowReviewDTO.java
@@ -0,0 +1,18 @@
+package com.austral.bookin.dto.review;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShowReviewDTO {
+
+    private Long id;
+    private int stars;
+    private String comment;
+    private Long userId;
+    private String userFirstName;
+    private String userLastName;
+}

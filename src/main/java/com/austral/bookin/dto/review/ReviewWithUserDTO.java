fc397b9689f1dcc74f3d49f28db44f56455b6379
Brian Froschauer <bafroschauer@gmail.com>
2020-10-03 12:27:52 -0300

    rename entity

--- src/main/java/com/austral/bookin/dto/review/ReviewWithUserDTO.java
+++ src/main/java/com/austral/bookin/dto/review/ReviewWithUserDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ShowReviewDTO {
+public class ReviewWithUserDTO {
 
     private Long id;
     private int stars;

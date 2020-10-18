190963084f2fd4fc150f081c02e72dd91dc7c6cf
Katia Cammisa <katia.cammisa@ing.austral.edu.ar>
2020-10-18 20:18:00 -0300

    Fixing language, genre and nationality spec to send many options

--- src/main/java/com/austral/bookin/specification/SearchBookSpecification.java
+++ src/main/java/com/austral/bookin/specification/SearchBookSpecification.java
@@ -2,7 +2,7 @@ package com.austral.bookin.specification;
 
 import com.austral.bookin.entity.Book;
 import net.kaczmarzyk.spring.data.jpa.domain.Between;
-import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.In;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
@@ -10,8 +10,8 @@ import org.springframework.data.jpa.domain.Specification;
 
 @And({
         @Spec(path = "title", params = "title", spec = Like.class),
-        @Spec(path = "genre", params = "genre", spec = Equal.class),
-        @Spec(path = "language", params = "language", spec = Equal.class),
+        @Spec(path = "genre", params = "genre", spec = In.class),
+        @Spec(path = "language", params = "language", spec = In.class),
         @Spec(path = "date", params = {"publishedAfter", "publishedBefore"}, spec = Between.class),
         @Spec(path = "stars", params = {"lowRange", "topRange"}, spec = Between.class)
 })

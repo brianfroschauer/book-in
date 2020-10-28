342e16c2482aebdc661cb03babb0839909eb93c0
katiacammisa <43763141+katiacammisa@users.noreply.github.com>
2020-10-28 00:03:59 -0300

    Add loaders (#49)
    * Adding authors
    
    * Adding books
    
    * Adding reviews
    
    * Minor change to fit front's convention

--- src/main/java/com/austral/bookin/loader/AuthorLoader.java
+++ src/main/java/com/austral/bookin/loader/AuthorLoader.java
@@ -31,31 +31,65 @@ public class AuthorLoader implements CommandLineRunner, Ordered {
                 "Mark",
                 "Twain",
                 "US",
-                new Date(-65, Calendar.SEPTEMBER, 30)
-        );
+                new Date(-65, Calendar.SEPTEMBER, 30));
+
         final Author author2 = new Author(
                 "Charles",
                 "Dickens",
                 "GB",
-                new Date(-88, Calendar.FEBRUARY, 7)
-        );
+                new Date(-88, Calendar.FEBRUARY, 7));
+
         final Author author3 = new Author(
                 "William",
                 "Shakespeare",
                 "GB",
-                new Date(65, Calendar.JULY, 31)
-        );
+                new Date(65, Calendar.JULY, 31));
+
         final Author author4 = new Author(
                 "Joanne",
                 "Rowling",
                 "GB",
-                new Date(65, Calendar.JULY, 31)
-        );
+                new Date(65, Calendar.JULY, 31));
+
+        final Author author5 = new Author(
+                "Rick",
+                "Riordan",
+                "US",
+                new Date(64, Calendar.JUNE, 5));
+
+        final Author author6 = new Author(
+                "Jane",
+                "Austen",
+                "US",
+                new Date(-125, Calendar.DECEMBER, 16));
+
+        final Author author7 = new Author(
+                "Lauren",
+                "Oliver",
+                "US",
+                new Date(82, Calendar.NOVEMBER, 8));
+
+        final Author author8 = new Author(
+                "Robert",
+                "Fisher",
+                "US",
+                new Date(82, Calendar.JUNE, 4));
+
+        final Author author9 = new Author(
+                "Mary",
+                "Stewart",
+                "GB",
+                new Date(16, Calendar.JULY, 2));
 
         authorService.save(author1, new MockMultipartFile("mark_twain.jpg", new FileInputStream(new File("src/main/resources/assets/authors/mark_twain.jpg"))));
         authorService.save(author2, new MockMultipartFile("charles_dickens.jpg", new FileInputStream(new File("src/main/resources/assets/authors/charles_dickens.jpg"))));
         authorService.save(author3, new MockMultipartFile("william_shakespeare.jpg", new FileInputStream(new File("src/main/resources/assets/authors/william_shakespeare.jpg"))));
         authorService.save(author4, new MockMultipartFile("jk_rowling.jpg", new FileInputStream(new File("src/main/resources/assets/authors/jk_rowling.jpg"))));
+        authorService.save(author5, new MockMultipartFile("rick_riordan.jpg", new FileInputStream(new File("src/main/resources/assets/authors/rick_riordan.jpg"))));
+        authorService.save(author6, new MockMultipartFile("jane_austen.jpg", new FileInputStream(new File("src/main/resources/assets/authors/jane_austen.jpg"))));
+        authorService.save(author7, new MockMultipartFile("lauren_oliver.jpg", new FileInputStream(new File("src/main/resources/assets/authors/lauren_oliver.jpeg"))));
+        authorService.save(author8, new MockMultipartFile("robert_fisher.jpg", new FileInputStream(new File("src/main/resources/assets/authors/robert_fisher.jpg"))));
+        authorService.save(author9, new MockMultipartFile("mary_stewart.jpeg", new FileInputStream(new File("src/main/resources/assets/authors/mary_stewart.jpeg"))));
     }
 
     @Override
